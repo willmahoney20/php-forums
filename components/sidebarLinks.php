0d48e7487845fe6539d65fe2aabb7ccaed38a731
Will Mahoney <will.mahoney3@gmail.com>
2024-03-06 09:24:00 +0000

    Added second sidebarLinks section

--- components/sidebarLinks.php
+++ components/sidebarLinks.php
@@ -1,50 +1,125 @@
-<div class="w-full flex flex-col py-4 px-1 mb-6">
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/explore.png" alt="Logo">
+<div class="w-full">
+    <div class="w-full flex flex-col py-4 px-1 mb-4">
+        <div class="flex flex-row items-center mb-6">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/explore.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                Explore
+            </h5>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            Explore
-        </h5>
+        <div class="flex flex-row items-center mb-6">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/new.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                New Posts
+            </h5>
+        </div>
+        <div class="flex flex-row items-center mb-6">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/mic.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                Podcasts
+            </h5>
+        </div>
+        <div class="flex flex-row items-center mb-6">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/following.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                Following
+            </h5>
+        </div>
+        <div class="flex flex-row items-center mb-6">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/chat.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                Threads
+            </h5>
+        </div>
+        <div class="flex flex-row items-center">
+            <div class="w-6 h-6">
+                <img class="h-6 w-6" src="../assets/about.png" alt="Logo">
+            </div>
+            <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
+                About Us
+            </h5>
+        </div>
     </div>
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/new.png" alt="Logo">
+    
+    <hr />
+    
+    <div class="w-full flex flex-col py-4 px-1 mt-4">
+        <div class="flex flex-row justify-between items-center mb-6">
+            <h5 class="text-slate-300 text font-semibold mb-0">
+                Popular Tags
+            </h5>
+            <h5 class="text-green-500 text font-semibold mb-0">
+                See All
+            </h5>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            New Posts
-        </h5>
-    </div>
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/mic.png" alt="Logo">
+        <div class="flex flex-row items-center mb-6">
+            <div class="flex items-center justify-center bg-gradient-to-bl from-green-900 to-green-400 w-8 h-8 rounded-full">
+            </div>
+            <div class="flex flex-col">
+                <h5 class="text-slate-300 text-sm font-semibold ml-3 mb-0">
+                    #research
+                </h5>
+                <h5 class="text-slate-300 text-xs font-semibold ml-3 mb-0 opacity-70">
+                    64,258 posts
+                </h5>
+            </div>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            Podcasts
-        </h5>
-    </div>
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/following.png" alt="Logo">
+        <div class="flex flex-row items-center mb-6">
+            <div class="flex items-center justify-center bg-gradient-to-bl from-green-900 to-green-400 w-8 h-8 rounded-full">
+            </div>
+            <div class="flex flex-col">
+                <h5 class="text-slate-300 text-sm font-semibold ml-3 mb-0">
+                    #habituation
+                </h5>
+                <h5 class="text-slate-300 text-xs font-semibold ml-3 mb-0 opacity-70">
+                    58,971 posts
+                </h5>
+            </div>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            Following
-        </h5>
-    </div>
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/chat.png" alt="Logo">
+        <div class="flex flex-row items-center mb-6">
+            <div class="flex items-center justify-center bg-gradient-to-bl from-green-900 to-green-400 w-8 h-8 rounded-full">
+            </div>
+            <div class="flex flex-col">
+                <h5 class="text-slate-300 text-sm font-semibold ml-3 mb-0">
+                    #earplugs
+                </h5>
+                <h5 class="text-slate-300 text-xs font-semibold ml-3 mb-0 opacity-70">
+                    43,289 posts
+                </h5>
+            </div>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            Threads
-        </h5>
-    </div>
-    <div class="flex flex-row items-center mb-6">
-        <div class="w-6 h-6">
-            <img class="h-6 w-6" src="../assets/about.png" alt="Logo">
+        <div class="flex flex-row items-center mb-6">
+            <div class="flex items-center justify-center bg-gradient-to-bl from-green-900 to-green-400 w-8 h-8 rounded-full">
+            </div>
+            <div class="flex flex-col">
+                <h5 class="text-slate-300 text-sm font-semibold ml-3 mb-0">
+                    #lightshow
+                </h5>
+                <h5 class="text-slate-300 text-xs font-semibold ml-3 mb-0 opacity-70">
+                    17,541 posts
+                </h5>
+            </div>
         </div>
-        <h5 class="text-slate-300 text font-semibold ml-5 mb-0">
-            About Us
-        </h5>
+        <div class="flex flex-row items-center mb-6">
+            <div class="flex items-center justify-center bg-gradient-to-bl from-green-900 to-green-400 w-8 h-8 rounded-full">
+            </div>
+            <div class="flex flex-col">
+                <h5 class="text-slate-300 text-sm font-semibold ml-3 mb-0">
+                    #jre
+                </h5>
+                <h5 class="text-slate-300 text-xs font-semibold ml-3 mb-0 opacity-70">
+                    9,574 posts
+                </h5>
+            </div>
+        </div>
     </div>
 </div>
